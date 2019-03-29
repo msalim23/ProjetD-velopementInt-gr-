583e731e666aaf0ed15aad48552fde11f9db0d9a
MESALIM <mehdi.salim.23@gmail.com>
2019-03-29 13:50:13 +0100

    correction naming

--- src/main/java/com/icc/projetreservations/repository/UserRepository.java
+++ src/main/java/com/icc/projetreservations/repository/UserRepository.java
@@ -4,5 +4,5 @@ import com.icc.projetreservations.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    User getUserByLoginAndPassword(String login, String Password);
+    User getUserByLoginAndPassword(String login, String password);
 }
